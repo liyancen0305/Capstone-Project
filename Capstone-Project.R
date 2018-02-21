03ebcfbe37f9fc16f98a3a0735e88e7380d86306
liyancen0305 <cenliyan93@gmail.com>
2018-02-21 13:10:21 -0800

    test

--- Capstone-Project.R
+++ Capstone-Project.R
@@ -1,10 +1,10 @@
 #####################################################################  Project Proposal ########################################################
 #1. What is the problem you want to solve?
-  #An investor is interested in investment property in both Austin, Texas and Los Angeles County. With respect to the data collected for a Metropolitan Area, Round Rock and Los Angeles County, over the years, we present the client the comparison between two cities regarding the investing purpose. 
+  # An investor is interested in investment property in both Austin, Texas and Los Angeles County. With respect to the data collected for a Metropolitan Area, Round Rock and Los Angeles County, over the years, we present the client the comparison between two cities regarding the investing purpose. 
 #2. Who is your client and why do they care about this problem? In other words, what will your client DO or DECIDE based on your analysis that they wouldn’t have otherwise?
-  #My client is interested in invest housing in Los Angeles, and Round Rock, Texas. I will analyze data on the “house price index”, and “unemployment” and give a forecast for both cities.
+  # My client is interested in invest housing in Los Angeles, and Round Rock, Texas. I will analyze data on the “house price index”, and “unemployment” and give a forecast for both cities.
 #3 What data are you going to use for this? How will you acquire this data?
-  #Austin-Round Rock
+  # Austin-Round Rock
     # House Price Index: https://fred.stlouisfed.org/series/ATNHPIUS12420Q
     # Unemployment Rate: https://fred.stlouisfed.org/series/AUST448URN?utm_source=series_page&utm_medium=related_content&utm_term=related_resources&utm_campaign=categories
     # Demographics: https://en.wikipedia.org/wiki/Round_Rock,_Texas
